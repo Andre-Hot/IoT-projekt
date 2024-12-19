1e75b5ea2b9e26d63f12cd395cfc33e1a96e80dd
Andrew S <123978520+Andre-Hot@users.noreply.github.com>
2024-12-19 11:06:59 +0100

    Update main.py
    GPS code was missing

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ from gpio_lcd import GpioLcd
 from machine import UART
 from gps_simple import GPS_SIMPLE
 from uthingsboard.client import TBDeviceMqttClient
+from machine import PWM, DAC
+from machine import Pin, time_pulse_us
+import secrets
 
+trigger = Pin(4, Pin.OUT)
+echo = Pin(5, Pin.IN)
+buzzer = PWM(14, Pin.OUT)
+pin_led1 = 26
+pin_led3 = 13
 gps_port = 2                                 # ESP32 UART port, Educaboard ESP32 default UART port
 gps_speed = 9600
 
@@ -16,7 +24,7 @@ gps = GPS_SIMPLE(uart)
 client = TBDeviceMqttClient(secrets.SERVER_IP_ADDRESS, access_token = secrets.ACCESS_TOKEN)
 client.connect()  
 
-i2c = I2C(0)
+i2c = I2C(0, freq=100000)
 imu = MPU6050(i2c)
 dht11 = DHT11(Pin(0, Pin.IN))
 
@@ -30,6 +38,20 @@ U2 = 4.2
 
 a = (U1-U2)/(adc1-adc2)
 b = U2 - a*adc2
+
+def measure_distance():
+    #Send 10us puls til trigger pin
+    trigger.off()
+    time.sleep_us(2)
+    trigger.on()
+    time.sleep_us(10)
+    trigger.off()
+    
+    #Mål den tid det tager for echo pin at modtage signal
+    duration = time_pulse_us(echo, 1, 30000)
+    
+    distance_cm = (duration * 0.0343) / 2
+    return distance_cm
 
 def batt_voltage(adc_v):
     u_batt = a*adc_v+b
@@ -59,6 +81,20 @@ custom_chr = bytearray([0b00111,
                         0b00000,
                         0b00000])
 
+def get_lat_lon():
+    lat = lon = None                       # create lat and lon variable with None as default value
+    if gps.receive_nmea_data():            # check if data is recieved
+                                           # check if the data is valid
+        if gps.get_latitude() != -999.0 and gps.get_longitude() != -999.0 and gps.get_validity() == "A":
+            lat = str(gps.get_latitude())  # store latitude in lat variable
+            lon = str(gps.get_longitude()) # stor longitude in lon variable
+            return lat, lon                # multiple return values, needs unpacking or it will be tuple format
+        else:                              # if latitude and longitude are invalid
+            print(f"GPS data to server not valid:\nlatitude: {lat}\nlongtitude: {lon}")
+            return False
+    else:
+        return False
+
 
 while True:
     if gps.receive_nmea_data():
@@ -74,9 +110,11 @@ while True:
         lcd.putstr(f"km/t: {gps.get_speed():.2f}")
         lcd.move_to(11, 3)
         lcd.putstr(f"Ret:{gps.get_course():.1f}\n")
+    try:    
+        print(imu.get_values())#.get("temperature celsius"))
+    except:
+        print('error reading imu')
         
-    print(imu.get_values())#.get("temperature celsius"))
-    
     dht11.measure()
     temperature = dht11.temperature()
     lcd.clear()
@@ -104,7 +142,10 @@ while True:
     lcd.move_to(10, 0)
     lcd.putstr(f"Bat: {percent:.1f}%")
     
-                                  # store telemetry in dictionary      
+    lat_lon = get_lat_lon()
+    if not lat_lon:
+        lat_lon = [0,0]
+    # store telemetry in dictionary      
     telemetry = {'latitude': lat_lon[0],
                  'longitude': lat_lon[1],
                  'Battery': percent,
@@ -113,5 +154,4 @@ while True:
                  'temperatur':temperature}
 
     client.send_telemetry(telemetry)
-    
     
